6d577e2ec891bd40be7e37ae9c9a682520b3272f
wandy matshete <wandymafanedzandymafanedza@gmail.com>
2022-09-12 18:19:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
-*main - print the spring in the put function
-*
-* Description: using the main function
-*this program prints "Programming is like building a multilingual puzzle
-*Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-   puts("\"programming is like building a multilingual puzzle");return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
+
