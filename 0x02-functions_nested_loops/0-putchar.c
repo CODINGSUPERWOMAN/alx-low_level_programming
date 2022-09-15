f9f62454f4f68468009b3983120685e58a7c853e
wandy matshete <wandymafanedzandymafanedza@gmail.com>
2022-09-14 21:03:47 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,43 @@
 #include "main.h"
+
+
+
 /**
- * main - Entry point
- *
- * Return: Alway 0 (Sucess)
+
+ * main - print putchar
+
+ * Return: always 0
+
  */
 
-int main(void)
-{
-       char *h = "_putchar\n":
 
-while (*h)
-	{
-		_putchar(*h);
-		h++;
-	}
+
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+
+
 	return (0);
+
+
+
 }
